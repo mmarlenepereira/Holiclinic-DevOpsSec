de7b4016e7bc17d39dfac8ccd203fb157e80fb11
mmarlenepereira <x22115234@student.ncirl.ie>
2023-03-27 19:30:50 +0100

    tests

--- __tests__/test_booking.js
+++ __tests__/test_booking.js
@@ -1,10 +1,42 @@
-const app = require('../app');
-const request = require('supertest');
+const puppeteer = require('puppeteer');
 
-describe('Booking page', () => {
-  test('should display the Calendly form', async () => {
-    const res = await request(app).get('/booking.html');
-    expect(res.status).toBe(200);
-    expect(res.text).toContain('calendly-widget');
+describe('Booking Page', () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test('Page Loads', async () => {
+    await page.goto('http://localhost:4000/booking.html');
+    const pageTitle = await page.title();
+    expect(pageTitle).toBe('Booking Page');
+  });
+
+  test('Can Book Appointment', async () => {
+    await page.goto('http://localhost:4000/booking.html');
+    await page.type('#name', 'John Doe');
+    await page.type('#email', 'johndoe@gmail.com');
+    await page.type('#date', '2023-03-30');
+    await page.click('#book-appointment');
+    const successMessage = await page.$eval('#success-message', el => el.innerText);
+    expect(successMessage).toBe('Appointment booked successfully!');
+  });
+
+  test('Can Cancel Appointment', async () => {
+    await page.goto('http://localhost:4000/booking.html');
+    await page.type('#name', 'John Doe');
+    await page.type('#email', 'johndoe@gmail.com');
+    await page.type('#date', '2023-03-30');
+    await page.click('#book-appointment');
+    await page.click('#cancel-appointment');
+    const successMessage = await page.$eval('#success-message', el => el.innerText);
+    expect(successMessage).toBe('Appointment cancelled successfully!');
   });
 });
