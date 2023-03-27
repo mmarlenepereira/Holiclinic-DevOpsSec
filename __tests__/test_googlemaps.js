3024cd46de2002c962722f94b94a76ca42ed79a7
mmarlenepereira <x22115234@student.ncirl.ie>
2023-03-27 19:57:03 +0100

    new test google maps

--- __tests__/test_googlemaps.js
+++ __tests__/test_googlemaps.js
@@ -1,22 +1,28 @@
 const puppeteer = require('puppeteer');
 
-  describe('Page About Us', () => {
-    let browser;
+describe('Google Maps', () => {
+  let browser;
+  let page;
 
-    beforeAll(async () => {
-      browser = await puppeteer.launch();
-    });
-
-    afterAll(async () => {
-      await browser.close();
-    });
-
-    test('should display Google Maps', async () => {
-      const page = await browser.newPage();
-      await page.goto('/aboutus.html');
-      await page.waitForSelector('#google-maps');
-      const map = await page.$('#google-maps');
-      expect(map).toBeDefined();
-    });
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
   });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+
+  test('should load Google Maps', async () => {
+    await page.goto('/contactus');
+    await page.waitForSelector('#map');
+
+    const mapTitle = await page.title();
+    expect(mapTitle).toContain('Google Maps');
+
+    const mapIframe = await page.$('#map iframe');
+    expect(mapIframe).not.toBeNull();
+  });
+});
+
 
