93aaf12d1a4aa6624e78675155a64ae9950cb9a9
mmarlenepereira <x22115234@student.ncirl.ie>
2023-03-27 18:38:29 +0100

    public folder with all html files, app.js, bin/www, and package.json

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+  console.log('Server started on port 4000');
+});
+
 // Set up middleware and routes
 module.exports = app;
+
+
