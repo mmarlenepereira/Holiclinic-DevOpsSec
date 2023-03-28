76bc18b68b70241e33eb4688553a599f009f259d
mmarlenepereira <x22115234@student.ncirl.ie>
2023-03-28 20:02:19 +0100

    new booking test

--- __tests__/test_booking.js
+++ __tests__/test_booking.js
@@ -1,17 +1,22 @@
-//tests whether there is a Calendly script on the page or not
+//checks for a Calendly script/Calendly widget on the Booking page
 
 const request = require('supertest');
-const assert = require('assert');
+const cheerio = require('cheerio');
+const app = require('../app');
 
-describe('Booking page', function() {
-  it('should include the Calendly script', function(done) {
-    request('http://localhost:4000')
-      .get('/booking.html')
-      .expect(200)
-      .end(function(err, res) {
-        if (err) return done(err);
-        assert(res.text.includes('calendly.com/assets/external/widget.js'), 'Calendly script is not included');
-        done();
-      });
+describe('Booking page', () => {
+  const agent = request.agent(app);
+
+  test('Calendly widget should exist and be working', async () => {
+    const response = await agent.get('/booking.html');
+    expect(response.status).toBe(200);
+
+    const $ = cheerio.load(response.text);
+    const calendlyWidget = $('.calendly-inline-widget');
+    expect(calendlyWidget.length).toBe(1);
+
+    const widgetScript = $('script[src="https://assets.calendly.com/assets/external/widget.js"]');
+    expect(widgetScript.length).toBe(1);
   });
 });
+
